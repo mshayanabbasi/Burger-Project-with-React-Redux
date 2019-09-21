3244e3cadbaaa8b19b04b2cf785465eaa18a7f6b
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-09-21 14:46:52 +0500

    add rootReducer

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import './index.css';
 import thunk from 'redux-thunk'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import burgerBuilder from './store/reducers/burgerBuilder'
+import burgerBuilderReducer from './store/reducers/burgerBuilder'
+import orderReducer from './store/reducers/order'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilder, composeEnhancers(
+const rootReducer = combineReducers({
+	burgerBuilder: burgerBuilderReducer,
+	order: orderReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
 		applyMiddleware(thunk)
 ))
 
