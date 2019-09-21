f3a31d2aa1c1ee9df69b9929aae666f5f6413a17
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-09-21 13:43:00 +0500

    changes

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes'
-
+import axios from '../../axios-orders'
 export const addIngredient = (name) => {
 	return {
 		type: actionTypes.ADD_INGREDIENT,
@@ -11,5 +11,30 @@ export const removeIngredient = (name) => {
 	return {
 		type: actionTypes.REMOVE_INGREDIENT,
 		ingredientName: name
+	}
+}
+
+export const setIngredient = (ingredients) => {
+	return {
+		type: actionTypes.SET_INGREDIENT,
+		ingredients: ingredients
+	}
+}
+
+export const fetchIngredientFailed = () => {
+	return {
+		type: actionTypes.FETCH_INGREDIENT_FAILED
+	}
+}
+
+export const initIngredient = () => {
+	return dispatch => {
+		axios.get( 'https://react-my-burger-15db7.firebaseio.com/ingredients.json' )
+		.then( response => {
+			dispatch(setIngredient(response.data))
+		} )
+		.catch( error => {
+			dispatch(fetchIngredientFailed())
+		} );
 	}
 }
