c46668e9388df1b5c90ee4e5280780adace4ed23
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-10-13 15:18:10 +0500

    changes

---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,12 @@
+import axios from '../../axios-orders'
+import { put } from 'redux-saga/effects'
+import * as actions from '../actions/index'
+export function* initIngredientsSaga(action) {
+    const response = yield axios.get( 'https://react-my-burger-15db7.firebaseio.com/ingredients.json' )
+        try {
+			yield put(actions.setIngredient(response.data))
+        }
+		catch(error){
+			yield put(actions.fetchIngredientFailed())
+        }   
+}
