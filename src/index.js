9abbadd3845faca8fd4524df352857dce999a9dd
mshayanabbasi <mshayanabbasi98@gmail.com>
2019-10-13 21:27:36 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder'
 import orderReducer from './store/reducers/order'
 import authReducer from './store/reducers/auth'
-import { watchAuth, watchBurgerBuilder } from './store/sagas'
+import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas'
 
 const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
 
@@ -29,6 +29,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 sagaMiddleware.run(watchAuth)
 sagaMiddleware.run(watchBurgerBuilder)
+sagaMiddleware.run(watchOrder)
 const app = (
 	<Provider store={store}>
     	<Router>
